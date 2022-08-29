4375e146e2846d7572a1c1e3abf7a7846dcbc17b
Jeffrey Wang <52841456+hjeffreywang@users.noreply.github.com>
2022-08-29 13:05:03 -0700

    Add files via upload

--- filepaths.py
+++ filepaths.py
@@ -1,3 +1,6 @@
-AUDIO_VIDEO_TUPLE_LIST=[("E4 Scott.mp3", 1),("E4 - Christine.mp3", 1), ("E4 - Crystal.mp3",2),("E4 - Chukwu.mp3",2)]
 VIDEO_FILEPATH_LIST= ["E4-M.mp4","E4-Left.mp4","E4-R.mp4"]
+AUDIO_VIDEO_TUPLE_LIST=[("E4 Scott.mp3", 1),("E4 - Christine.mp3", 1), ("E4 - Crystal.mp3",2),("E4 - Chukwu.mp3",2)]
 
+
+# ["video file middle","video file1","video file2"]
+#("audio file1", the position of the connected video file in the list IE:1 for video file1)
